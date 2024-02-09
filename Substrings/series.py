47d099cdebb89bfb27408e8a5b079cef1f39b799
rifat <rifatshaykhov@gmail.com>
2024-02-09 21:02:35 +0400

    Тесты пройдены. Убрал лишние циклы, уменьшил код.

--- Substrings/series.py
+++ Substrings/series.py
@@ -6,19 +6,9 @@ def slices(series, length):
     elif length == 0:
         raise ValueError('slice length cannot be zero')
     elif len(series) > 0:
-        s = list(series)
         if len(series) >= length:
             a = []
-            if length == 1:
-                return s
-            elif length == len(s):
-                return [series]
-            else:
-                count = len(s) - length + 1
-                while count > 0:
-                    for i in range(count):
-                        a.append(series[i:length+i])
-                        count -= 1
-                return a
+            [a.append(series[i:length + i]) for i in range(len(series) - length + 1)]
+            return a
         else:
             raise ValueError('slice length cannot be greater than series length')
